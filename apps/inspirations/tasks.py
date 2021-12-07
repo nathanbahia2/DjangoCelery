99274207dbb3d626752ece9c52d4b6490628eca2
nathanbahia2 <nathan@datatype.com.br>
2021-12-07 12:14:30 -0300

    Testa novo tipo de fazer uma chamada para tarefas do celery

--- apps/inspirations/tasks.py
+++ apps/inspirations/tasks.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from django_celery.celery import app
 
 
-@app.task(name="First Celery Task")
+@app.task(name="task_hello_world", queue="queue_hello_world")
 def hello_world():
-    print('HELLO, WORLD!!! IT WORKET!!! ')
+    print(f'HELLO, WORLD!!! IT WORKET!!! IT"S { datetime.now().isoformat() } RIGHT NOW')
