f478a4d511768da794bfeca76d8f5fa8c777f9ce
nathanbahia2 <nathan@datatype.com.br>
2021-12-07 13:25:01 -0300

    Ajustes nas configurações do Celery

--- apps/inspirations/tasks.py
+++ apps/inspirations/tasks.py
@@ -3,12 +3,15 @@ from datetime import datetime
 from django_celery.celery import app
 from celery.utils.log import get_task_logger
 
+from apps.inspirations.models import Message
+
 
 logger = get_task_logger(__name__)
 
 
 @app.task(name="task_hello_world", queue="queue_geral")
 def hello_world(*args, **kwargs):
-    logger.info(f'HELLO, WORLD!!! IT WORKET!!! IT"S { datetime.now().isoformat() } RIGHT NOW')
-    logger.info(args)
-    logger.info(kwargs)
+    Message.objects.create(
+        author='CELERY',
+        message=f'IT WORKET!!! IT"S { datetime.now().isoformat() } RIGHT NOW'
+    )
