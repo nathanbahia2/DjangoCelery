6a7795d2d8cd29a3539a567ded6774c9a145dbff
nathanbahia2 <nathan@datatype.com.br>
2021-12-07 12:55:24 -0300

    Ajustes nas configurações do Celery

--- apps/inspirations/tasks.py
+++ apps/inspirations/tasks.py
@@ -4,5 +4,7 @@ from django_celery.celery import app
 
 
 @app.task(name="task_hello_world", queue="queue_geral")
-def hello_world():
+def hello_world(*args, **kwargs):
     print(f'HELLO, WORLD!!! IT WORKET!!! IT"S { datetime.now().isoformat() } RIGHT NOW')
+    print(args)
+    print(kwargs)
